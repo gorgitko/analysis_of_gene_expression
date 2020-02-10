f36077d75f32011b40bd5d7367f2c86238b3d84e
Jiri Novotny <jiri.novotny@img.cas.cz>
2020-02-10 13:22:19 +0100

    move function docstring to the top of function declaration

--- Exercises/age_library.R
+++ Exercises/age_library.R
@@ -1,3 +1,17 @@
+#' Calculate hiearchical clustering and plot dendrogram.
+#'
+#' Args:
+#'   m: Expression matrix (rows are features, columns are samples).
+#'   method_distance: Distance metric used for clustering. See ?dist.
+#'                    Can be also correlation metrix ("pearson", "kendall", "spearman"). See ?cor.
+#'   method_clustering: Clustering method. See ?hclust.
+#'   color_by: Vector of discrete values to colorize samples.
+#'             Length must be the same as number of columns in 'm'.
+#'   color_by_lab: Name of the color legend.
+#'   main: Main plot title.
+#'
+#' Returns:
+#'   Object of class 'dendrogram'.
 plot_hc <- function(
   m,
   method_distance = "euclidean",
@@ -6,21 +20,6 @@ plot_hc <- function(
   color_by_lab = "Group",
   main = "Hierarchical Clustering"
 ) {
-  #' Calculate hiearchical clustering and plot dendrogram.
-  #'
-  #' Args:
-  #'   m: Expression matrix (rows are features, columns are samples).
-  #'   method_distance: Distance metric used for clustering. See ?dist.
-  #'                    Can be also correlation metrix ("pearson", "kendall", "spearman"). See ?cor.
-  #'   method_clustering: Clustering method. See ?hclust.
-  #'   color_by: Vector of discrete values to colorize samples.
-  #'             Length must be the same as number of columns in 'm'.
-  #'   color_by_lab: Name of the color legend.
-  #'   main: Main plot title.
-  #'
-  #' Returns:
-  #'   Object of class 'dendrogram'.
-
   library(RColorBrewer)
   library(glue)
   library(magrittr)
@@ -73,16 +72,15 @@ plot_hc <- function(
   return(invisible(hc))
 }
 
+#' Select features (rows) with the highest variance across the samples (columns).
+#'
+#' Args:
+#'   m: Expression matrix (rows are features, columns are samples).
+#'   n_top_features: Number of top features.
+#'
+#' Returns:
+#'   Object 'm' subset to 'n_top_features' with the highest variance across the samples.
 select_var_features <- function(m, n_top_features) {
-  #' Select features (rows) with the highest variance across the samples (columns).
-  #'
-  #' Args:
-  #'   m: Expression matrix (rows are features, columns are samples).
-  #'   n_top_features: Number of top features.
-  #'
-  #' Returns:
-  #'   Object 'm' subset to 'n_top_features' with the highest variance across the samples.
-
   if (!is.infinite(n_top_features))
   {
     # Calculate the variance for each gene.
@@ -96,6 +94,19 @@ select_var_features <- function(m, n_top_features) {
   return(m)
 }
 
+#' Plots PCA of first three PC's, colorized by 'group'. Uses the base R graphics.
+#'
+#' Args:
+#'   m: Expression matrix (rows are features, columns are samples).
+#'   color_by: Vector of discrete values to colorize samples.
+#'             Length must be the same as number of columns in 'm'.
+#'   n_top_features: Number of top features with the highest variance across the samples.
+#'   center: Whether to center PCA. See ?prcomp.
+#'   scale.: Whether to scale PCA. See ?prcomp.
+#'   color_by_lab: Name of the color legend.
+#'
+#' Returns:
+#'   Object of class 'prcomp'.
 plot_pca <- function(
   m,
   color_by,
@@ -104,20 +115,6 @@ plot_pca <- function(
   scale. = TRUE,
   color_by_lab = "Group"
 ) {
-  #' Plots PCA of first three PC's, colorized by 'group'. Uses the base R graphics.
-  #'
-  #' Args:
-  #'   m: Expression matrix (rows are features, columns are samples).
-  #'   color_by: Vector of discrete values to colorize samples.
-  #'             Length must be the same as number of columns in 'm'.
-  #'   n_top_features: Number of top features with the highest variance across the samples.
-  #'   center: Whether to center PCA. See ?prcomp.
-  #'   scale.: Whether to scale PCA. See ?prcomp.
-  #'   color_by_lab: Name of the color legend.
-  #'
-  #' Returns:
-  #'   Object of class 'prcomp'.
-
   library(RColorBrewer)
   library(glue)
   library(magrittr)
@@ -153,7 +150,22 @@ plot_pca <- function(
   return(invisible(pca))
 }
 
-
+#' Using ggplot2, plot first three PCs of samples in expression matrix.
+#'
+#' Args:
+#'   m: Expression matrix (rows are features, columns are samples).
+#'   sample_data: Dataframe describing samples.
+#'   n_top_features: Number of top features with the highest variance across the samples.
+#'   color_by: Column name in sample_data to use for point coloring.
+#'   shape_by: Column name in sample_data to use for point shape.
+#'   label_by: Column name in sample_data to use for point labels.
+#'   point_size: Point size (numeric).
+#'   text_size: Label text size (numeric).
+#'   center: Whether to center PCA. See ?prcomp.
+#'   scale.: Whether to scale PCA. See ?prcomp.
+#'
+#' Returns:
+#'   list(pca = prcomp object, pca_df = combined dataframe of sample_data and PCs, plot = ggplot2 object)
 plot_pca_ggplot2 <- function(
   m,
   sample_data,
@@ -166,23 +178,6 @@ plot_pca_ggplot2 <- function(
   center = TRUE,
   scale. = TRUE
 ) {
-  #' Using ggplot2, plot first three PCs of samples in expression matrix.
-  #'
-  #' Args:
-  #'   m: Expression matrix (rows are features, columns are samples).
-  #'   sample_data: Dataframe describing samples.
-  #'   n_top_features: Number of top features with the highest variance across the samples.
-  #'   color_by: Column name in sample_data to use for point coloring.
-  #'   shape_by: Column name in sample_data to use for point shape.
-  #'   label_by: Column name in sample_data to use for point labels.
-  #'   point_size: Point size (numeric).
-  #'   text_size: Label text size (numeric).
-  #'   center: Whether to center PCA. See ?prcomp.
-  #'   scale.: Whether to scale PCA. See ?prcomp.
-  #'
-  #' Returns:
-  #'   list(pca = prcomp object, pca_df = combined dataframe of sample_data and PCs, plot = ggplot2 object)
-
   library(rlist)
   library(ggplot2)
 
@@ -235,7 +230,29 @@ plot_pca_ggplot2 <- function(
   return(list(pca = pca, pca_df = pca_df, plot = p))
 }
 
-
+#' Using GGally::ggpairs(), plot grid of PCA plots (PC1 vs. PC2, PC1 vs. PC3, PC2 vs. PC3, etc).
+#' When n_components == 2, use normal ggplot2.
+#'
+#' Args:
+#'   m: Expression matrix (rows are features, columns are samples).
+#'   sample_data: Dataframe describing samples.
+#'   output_file: File to save plot in.
+#'   n_components: Number of PCs to plot.
+#'   n_top_features: Number of top features with the highest variance across the samples.
+#'   color_by: Column name in sample_data to use for point coloring.
+#'   color_legend_lab: Name of the color legend.
+#'   shape_by: Column name in sample_data to use for point shape.
+#'   shape_legend_lab: Name of the shape legend.
+#'   label_by: Column name in sample_data to use for point labels.
+#'   point_size: Point size (numeric).
+#'   text_size: Label text size (numeric).
+#'   title: Plot title.
+#'   subtitle: Plot subtitle.
+#'   center: Whether to center PCA. See ?prcomp.
+#'   scale.: Whether to scale PCA. See ?prcomp.
+#'
+#' Returns:
+#'   list(pca = prcomp object, pca_df = combined dataframe of sample_data and PCs, plot = ggplot2 object)
 plot_pca_ggpairs <- function(
   m,
   sample_info,
@@ -254,30 +271,6 @@ plot_pca_ggpairs <- function(
   center = TRUE,
   scale. = TRUE
 ) {
-  #' Using GGally::ggpairs(), plot grid of PCA plots (PC1 vs. PC2, PC1 vs. PC3, PC2 vs. PC3, etc).
-  #' When n_components == 2, use normal ggplot2.
-  #'
-  #' Args:
-  #'   m: Expression matrix (rows are features, columns are samples).
-  #'   sample_data: Dataframe describing samples.
-  #'   output_file: File to save plot in.
-  #'   n_components: Number of PCs to plot.
-  #'   n_top_features: Number of top features with the highest variance across the samples.
-  #'   color_by: Column name in sample_data to use for point coloring.
-  #'   color_legend_lab: Name of the color legend.
-  #'   shape_by: Column name in sample_data to use for point shape.
-  #'   shape_legend_lab: Name of the shape legend.
-  #'   label_by: Column name in sample_data to use for point labels.
-  #'   point_size: Point size (numeric).
-  #'   text_size: Label text size (numeric).
-  #'   title: Plot title.
-  #'   subtitle: Plot subtitle.
-  #'   center: Whether to center PCA. See ?prcomp.
-  #'   scale.: Whether to scale PCA. See ?prcomp.
-  #'
-  #' Returns:
-  #'   list(pca = prcomp object, pca_df = combined dataframe of sample_data and PCs, plot = ggplot2 object)
-
   library(ggplot2)
   library(magrittr)
 
@@ -363,24 +356,23 @@ plot_pca_ggpairs <- function(
 }
 
 
+#' Plot heatmap using heatmap.2()
+#'
+#' Args:
+#'   m: Expression matrix (rows are features, columns are samples).
+#'   color_by: Vector of discrete values to colorize samples.
+#'             Length must be the same as number of columns in 'm'.
+#'   color_legend_lab: Name of the color legend.
+#'   main: Plot title.
+#'
+#' Returns:
+#'   logical
 plot_heatmap <- function(
   m,
   color_by = NULL,
   color_legend_lab = "Group",
   main = ""
 ) {
-  #' Plot heatmap using heatmap.2()
-  #'
-  #' Args:
-  #'   m: Expression matrix (rows are features, columns are samples).
-  #'   color_by: Vector of discrete values to colorize samples.
-  #'             Length must be the same as number of columns in 'm'.
-  #'   color_legend_lab: Name of the color legend.
-  #'   main: Plot title.
-  #'
-  #' Returns:
-  #'   logical
-
   library(RColorBrewer)
   library(gplots)
   library(magrittr)
@@ -405,7 +397,20 @@ plot_heatmap <- function(
   return(invisible(TRUE))
 }
 
-
+#' Plot heatmap using pheatmap()
+#'
+#' Args:
+#'   m: Expression matrix (rows are features, columns are samples).
+#'   sample_data: Dataframe describing samples.
+#'   feature_data: Dataframe describing features
+#'   column_color_by: Vector of column names of sample_data to color columns in heatmap.
+#'                    If NULL and sample_data is provided, all columns will be used for coloring.
+#'   row_color_by: Vector of column names of feature_data to color rows in heatmap.
+#'                 If NULL and feature_data is provided, all columns will be used for coloring.
+#'   main: Plot title.
+#'
+#' Returns:
+#'   pheatmap object
 plot_pheatmap <- function(
   m,
   sample_data = NULL,
@@ -414,21 +419,6 @@ plot_pheatmap <- function(
   row_color_by = NULL,
   main = ""
 ) {
-  #' Plot heatmap using pheatmap()
-  #'
-  #' Args:
-  #'   m: Expression matrix (rows are features, columns are samples).
-  #'   sample_data: Dataframe describing samples.
-  #'   feature_data: Dataframe describing features
-  #'   column_color_by: Vector of column names of sample_data to color columns in heatmap.
-  #'                    If NULL and sample_data is provided, all columns will be used for coloring.
-  #'   row_color_by: Vector of column names of feature_data to color rows in heatmap.
-  #'                 If NULL and feature_data is provided, all columns will be used for coloring.
-  #'   main: Plot title.
-  #'
-  #' Returns:
-  #'   pheatmap object
-
   if (!is.null(sample_data) && !is.null(column_color_by)) {
     sample_data <- sample_data[, column_color_by, drop = FALSE]
   }
@@ -440,7 +430,21 @@ plot_pheatmap <- function(
   pheatmap::pheatmap(m, annotation_col = sample_data, annotation_row = feature_data, main = main)
 }
 
-
+#' Create heatmap using heatmaply.
+#'
+#' Args:
+#'   m: Expression matrix (rows are features, columns are samples).
+#'   sample_data: Dataframe describing samples.
+#'   feature_data: Dataframe describing features
+#'   column_color_by: Vector of column names of sample_data to color columns in heatmap.
+#'                    If NULL and sample_data is provided, all columns will be used for coloring.
+#'   row_color_by: Vector of column names of feature_data to color rows in heatmap.
+#'                 If NULL and feature_data is provided, all columns will be used for coloring.
+#'   main: Plot title.
+#'   key.title: Main color legend title.
+#'
+#' Returns:
+#'   plotly object
 plot_heatmaply <- function(
   m,
   sample_data = NULL,
@@ -450,22 +454,6 @@ plot_heatmaply <- function(
   main = NULL,
   key.title = NULL
 ) {
-  #' Create heatmap using heatmaply.
-  #'
-  #' Args:
-  #'   m: Expression matrix (rows are features, columns are samples).
-  #'   sample_data: Dataframe describing samples.
-  #'   feature_data: Dataframe describing features
-  #'   column_color_by: Vector of column names of sample_data to color columns in heatmap.
-  #'                    If NULL and sample_data is provided, all columns will be used for coloring.
-  #'   row_color_by: Vector of column names of feature_data to color rows in heatmap.
-  #'                 If NULL and feature_data is provided, all columns will be used for coloring.
-  #'   main: Plot title.
-  #'   key.title: Main color legend title.
-  #'
-  #' Returns:
-  #'   plotly object
-
   # heatmaply cannot handle NULL parameters.
   # Could be this implemented better?
   params <- list(m, key.title = key.title)
@@ -485,7 +473,29 @@ plot_heatmaply <- function(
   do.call(heatmaply::heatmaply, params)
 }
 
-
+#' Plot boxplot of gene expression.
+#'
+#' Args:
+#'   plot_data: data.frame (long format)
+#'   x: Column to divide x-axis values to (e.g. sample groups).
+#'   y: Column to compute boxplots on y-axis.
+#'   feature_col: Column for facetting.
+#'   feature: Name of feature from which boxplots will be made.
+#'            If NULL, facet by feature_col (facet_by will be ignored).
+#'   facet_by: One or two columns used for facetting.
+#'   color_by: Column to use for boxplot and point coloring.
+#'   main: Main plot title.
+#'   add: Add something more to boxplots.
+#'        Allowed values are one or the combination of:
+#'        "none", "dotplot", "jitter", "boxplot", "point", "mean",
+#'        "mean_se", "mean_sd", "mean_ci", "mean_range", "median",
+#'        "median_iqr", "median_mad", "median_range".
+#'        See ?ggpubr::ggboxplot
+#'   point_size: Size of points inside boxplots.
+#'   do_t_test: Whether to do the t-test and display a p-value inside the plot.
+#'
+#' Returns:
+#'   ggplot2 object
 plot_boxplot_ggplot2 <- function(
   plot_data,
   x,
@@ -499,30 +509,6 @@ plot_boxplot_ggplot2 <- function(
   point_size = 2,
   do_t_test = TRUE
 ) {
-  #' Plot boxplot of gene expression.
-  #'
-  #' Args:
-  #'   plot_data: data.frame (long format)
-  #'   x: Column to divide x-axis values to (e.g. sample groups).
-  #'   y: Column to compute boxplots on y-axis.
-  #'   feature_col: Column for facetting.
-  #'   feature: Name of feature from which boxplots will be made.
-  #'            If NULL, facet by feature_col (facet_by will be ignored).
-  #'   facet_by: One or two columns used for facetting.
-  #'   color_by: Column to use for boxplot and point coloring.
-  #'   main: Main plot title.
-  #'   add: Add something more to boxplots.
-  #'        Allowed values are one or the combination of:
-  #'        "none", "dotplot", "jitter", "boxplot", "point", "mean",
-  #'        "mean_se", "mean_sd", "mean_ci", "mean_range", "median",
-  #'        "median_iqr", "median_mad", "median_range".
-  #'        See ?ggpubr::ggboxplot
-  #'   point_size: Size of points inside boxplots.
-  #'   do_t_test: Whether to do the t-test and display a p-value inside the plot.
-  #'
-  #' Returns:
-  #'   ggplot2 object
-
   library(ggplot2)
   library(glue)
   library(friendlyeval)
@@ -580,17 +566,15 @@ plot_boxplot_ggplot2 <- function(
   return(p)
 }
 
-
+#' Compute the M value of CP values.
+#'
+#' Args:
+#'  gene: Name of gene to compute the M value.
+#'  cp_m: Dataframe of CP values. Rows are genes and columns are samples.
+#'
+#' Returns:
+#'  M value (numeric)
 compute_m <- function(gene, cp_m) {
-  #' Compute the M value of CP values.
-  #'
-  #' Args:
-  #'  gene: Name of gene to compute the M value.
-  #'  cp_m: Dataframe of CP values. Rows are genes and columns are samples.
-  #'
-  #' Returns:
-  #'  M value (numeric)
-
   og <- setdiff(rownames(cp_m), gene)
 
   V <- apply(cp_m[og, ], 1, function(x) {sd(cp_m[gene, ] - x)})
@@ -599,22 +583,20 @@ compute_m <- function(gene, cp_m) {
   return(M)
 }
 
-
+#' For a single gene, test for statistical significance of difference in group means.
+#'
+#' Args:
+#'   gene: Name of gene to test.
+#'   gene_data: Dataframe in long format.
+#'   gene_col: Column with genes.
+#'   value_col: Column with values to test.
+#'   group_col: Column with sample groups. There must be exactly two different groups.
+#'   test: Statistical test to perform. It must have the same interface as t.test()
+#'   verbose: Whether to print test results.
+#'
+#' Returns:
+#'   htest object
 test_gene <- function(gene, gene_data, gene_col, value_col, group_col, test = t.test, verbose = TRUE) {
-  #' For a single gene, test for statistical significance of difference in group means.
-  #'
-  #' Args:
-  #'   gene: Name of gene to test.
-  #'   gene_data: Dataframe in long format.
-  #'   gene_col: Column with genes.
-  #'   value_col: Column with values to test.
-  #'   group_col: Column with sample groups. There must be exactly two different groups.
-  #'   test: Statistical test to perform. It must have the same interface as t.test()
-  #'   verbose: Whether to print test results.
-  #'
-  #' Returns:
-  #'   htest object
-
   library(glue)
   library(magrittr)
   library(friendlyeval)
@@ -643,20 +625,18 @@ test_gene <- function(gene, gene_data, gene_col, value_col, group_col, test = t.
   return(res)
 }
 
-
+#' For all genes in the input dataframe, test for statistical significance of difference in group means.
+#'
+#' Args:
+#'   gene_data: Dataframe in long format.
+#'   gene_col: Column with genes.
+#'   value_col: Column with values to test.
+#'   group_col: Column with sample groups. There must be exactly two different groups.
+#'   test: Statistical test to perform. It must have the same interface as t.test()
+#'
+#' Returns:
+#'   tibble object
 test_gene_table <- function(gene_data, gene_col, value_col, group_col, test = t.test) {
-  #' For all genes in the input dataframe, test for statistical significance of difference in group means.
-  #'
-  #' Args:
-  #'   gene_data: Dataframe in long format.
-  #'   gene_col: Column with genes.
-  #'   value_col: Column with values to test.
-  #'   group_col: Column with sample groups. There must be exactly two different groups.
-  #'   test: Statistical test to perform. It must have the same interface as t.test()
-  #'
-  #' Returns:
-  #'   tibble object
-
   library(magrittr)
 
   genes <- unique(gene_data[, gene_col, drop = TRUE])
@@ -676,16 +656,14 @@ test_gene_table <- function(gene_data, gene_col, value_col, group_col, test = t.
   ))
 }
 
-
+#' Return asterisks according to p-values.
+#'
+#' Args:
+#'   p_value: Vector of p-values.
+#'
+#' Returns:
+#'  character vector
 asterisk <- function(p_value) {
-  #' Return asterisks according to p-values.
-  #'
-  #' Args:
-  #'   p_value: Vector of p-values.
-  #'
-  #' Returns:
-  #'  character vector
-
   dplyr::case_when(
     is.na(p_value) ~ NA_character_,
     p_value < 0.001 ~ "***",
@@ -694,4 +672,3 @@ asterisk <- function(p_value) {
     TRUE ~ "NS"
   )
 }
-
