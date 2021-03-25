da93e2d503c210aebd59c2738b22b0dfd4e30660
Jiri Novotny <jiri.novotny@img.cas.cz>
2021-03-25 07:24:07 +0000

    initial commit

---
+++ Exercises/E02-intro_to_R/export.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+TO_EXPORT=`echo assignment/E02-intro_to_R_assignment.{html,Rmd} assignment/{p_heatmap.png,p2.png} ComplexHeatmap-intro images ggplot2_example.* intro_to_R.{html,Rmd} intro_to_Rmd.{html,knit.md,Rmd,utf8.md} intro_to_Rmd_tufte.html p_plotly.html tab_design.*`
+
+if [ -z "$1" ]; then
+  echo "Destination path is not set!"
+  exit 1
+fi
+
+echo "To export: $TO_EXPORT"
+cp -r --parents $TO_EXPORT $1
